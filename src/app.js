adef7f42241c854837158dadec86fa74bb0a7b93
just-jatinverma <vermajatin1727@gmail.com>
2025-06-23 22:37:06 +0530

    feat: implement user registration route and multer middleware for file uploads

--- src/app.js
+++ src/app.js
@@ -12,4 +12,13 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 
 app.use(express.static('public'));
 
+app.use(cookieParser());
+
+// import user router
+import userRouter from './routes/user.routes.js';
+
+// routes declaration
+
+app.use('/api/v1/users', userRouter);
+
 export { app };
