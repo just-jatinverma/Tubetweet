06925fcd211a7287350fe8f23267e88075b5a225
just-jatinverma <vermajatin1727@gmail.com>
2025-06-27 22:33:55 +0530

    feat: add JWT verification to refresh token and user-related routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logOutUser,
   refreshAccessToken,
   registerUser,
+  changePassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  watchHistory,
 } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -28,6 +35,20 @@ router.route('/login').post(loginUser);
 
 router.route('/logout').post(verifyJWT, logOutUser);
 
-router.route('/refresh-token').post(refreshAccessToken);
+router.route('/refresh-token').post(verifyJWT, refreshAccessToken);
+
+router.route('/change-password').post(verifyJWT, changePassword);
+
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+
+router.route('./update-details').patch(verifyJWT, updateAccountDetails);
+
+router.route('./avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+
+router.route('./cover-Image').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile);
+
+router.route('./watch-history').get(verifyJWT, watchHistory);
 
 export default router;
