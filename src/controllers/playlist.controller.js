f8f0aab58a0846113395c9f48ea14c9570a70212
just-jatinverma <vermajatin1727@gmail.com>
2025-06-30 21:07:16 +0530

    feat: implement create, get user playlists, and get playlist by ID functionalities in playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,11 +3,43 @@ import { ApiError } from '../utils/apiError';
 import { apiResponse } from '../utils/apiResponse';
 import { Playlist } from '../models/playlist.model';
 
-const createPlaylist = asyncHandler(async (req, res) => {});
+const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  const userId = req.user._id;
 
-const getUserPlaylists = asyncHandler(async (req, res) => {});
+  if (!name) {
+    throw new ApiError(400, 'name not found');
+  }
 
-const getPlaylistById = asyncHandler(async (req, res) => {});
+  const playlist = await Playlist.create({
+    name,
+    description: description,
+    videos: [],
+    owner: userId,
+  });
+
+  return res.status(200).json(new apiResponse(200, playlist, 'blank playlist created'));
+});
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const playlists = await Playlist.find({ owner: userId }).sort({ createdAt: -1 });
+
+  return res.status(200).json(new apiResponse(200, playlists, 'All playlists for user'));
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+
+  const playlist = await Playlist.findById(playlistId).populate('videos');
+
+  if (!playlist) {
+    throw new ApiError(40, 'Playlist not found');
+  }
+
+  return res.status(200).json(new apiResponse(200, playlist, 'requested playlist'));
+});
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {});
 
