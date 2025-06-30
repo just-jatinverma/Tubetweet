4e128c0e01fb982f575dac5d45b1f2c00bb1bc1c
just-jatinverma <vermajatin1727@gmail.com>
2025-06-30 22:06:16 +0530

    feat: implement add, remove, delete, and update playlist functionalities with error handling

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -41,13 +41,103 @@ const getPlaylistById = asyncHandler(async (req, res) => {
   return res.status(200).json(new apiResponse(200, playlist, 'requested playlist'));
 });
 
-const addVideoToPlaylist = asyncHandler(async (req, res) => {});
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  const { videoId, playlistId } = req.params;
+  const userId = req.user._id;
 
-const removeVideoFromPlaylist = asyncHandler(async (req, res) => {});
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, 'playlist not found');
+  }
 
-const deletePlaylist = asyncHandler(async (req, res) => {});
+  if (playlist.owner.toString() != userId.toString()) {
+    throw new ApiError(403, 'forbidden');
+  }
 
-const updatePlaylist = asyncHandler(async (req, res) => {});
+  if (playlist.videos.some((id) => id.toString() === videoId.toString())) {
+    throw new ApiError(400, 'already in playlist');
+  } else {
+    playlist.videos.push(videoId);
+  }
+
+  await playlist.save();
+
+  return res.status(200).json(new apiResponse(200, playlist, 'new playlist'));
+});
+
+const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
+  const { videoId, playlistId } = req.params;
+  const userId = req.user._id;
+
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, 'playlist not found');
+  }
+
+  if (playlist.owner.toString() != userId.toString()) {
+    throw new ApiError(403, 'forbidden');
+  }
+
+  const indexOFVideo = playlist.videos.findIndex((id) => id.toString() === videoId.toString());
+
+  if (indexOFVideo === -1) {
+    throw new ApiError(400, 'Video not found in playlist');
+  }
+
+  playlist.videos.splice(indexOFVideo, 1);
+
+  await playlist.save();
+
+  return res.status(200).json(new apiResponse(200, playlist, 'removed from playlist'));
+});
+
+const deletePlaylist = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+  const userId = req.user._id;
+
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, 'playlist not found');
+  }
+
+  if (playlist.owner.toString() != userId.toString()) {
+    throw new ApiError(403, 'forbidden');
+  }
+
+  await Playlist.findByIdAndDelete(playlistId);
+
+  return res.status(200).json(new apiResponse(200, {}, 'playlist deleted'));
+});
+
+const updatePlaylist = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params;
+  const userId = req.user._id;
+
+  const { name, description } = req.body;
+
+  if (!name && !description) {
+    throw new ApiError(400, 'At least one field is required');
+  }
+
+  const playlist = await Playlist.findById(playlistId);
+  if (!playlist) {
+    throw new ApiError(404, 'playlist not found');
+  }
+
+  if (playlist.owner.toString() != userId.toString()) {
+    throw new ApiError(403, 'forbidden');
+  }
+
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    playlistId,
+    {
+      $set: { name, description },
+    },
+    { new: true }
+  );
+
+  return res.status(200).json(new apiResponse(200, updatedPlaylist, 'playlist updated'));
+});
 
 export {
   addVideoToPlaylist,
